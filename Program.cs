85ea746e020a79a848c602193a938d3e17f4572f
HalilKakut <halil.kakut@hotmail.com>
2022-01-19 12:25:05 +0300

    add while and foreach loop

--- Program.cs
+++ Program.cs
@@ -42,11 +42,42 @@ namespace Donguler_For_Loop
             //     Console.WriteLine(t);
             // }
 
-            for(int t=1; t<=10; t++)
+            // for(int t=1; t<=10; t++)
+            // {
+            //     if(t == 4)
+            //         continue; 
+            //     Console.WriteLine(t);
+            // }
+            Console.WriteLine("******While******");
+            //While 1 den başlayarak console dan girilen sayıya kadar ortalama hesaplama
+
+            Console.Write("Bir Sayı giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
             {
-                if(t == 4)
-                    continue; 
-                Console.WriteLine(t);
+                 toplam+=sayac;
+                 sayac++;
+            }
+
+            //Console.WriteLine(toplam / sayi);
+            // 'a' dan 'z' ye kadar karakter yazma
+            
+            // char character = 'a';
+
+            // while (character < 'z')
+            // {
+            //      Console.Write(character);
+            //      character++;
+            // }
+
+            Console.WriteLine("******Foreach******");
+            string[] cars = {"BMW","Ford","Toyota","Mercedes"};
+
+            foreach (var item in cars)
+            {
+                Console.WriteLine(item);
             }
         }
     }
